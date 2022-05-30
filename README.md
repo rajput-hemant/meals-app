f93109d5309e58cfe7f717dd04c4d468ccc77059
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-31 00:32:32 +0530

    init
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ README.md
@@ -0,0 +1,17 @@
+# meals-app
+
+## Building from Source
+
+- If you don't have Flutter SDK installed, please visit official [Flutter](https://flutter.dev/) site.
+- Fetch latest source code from master branch.
+
+```console
+rajput-hemant@arch:~$ git clone https://github.com/rajput-hemant/meals-app
+```
+
+- Run the app with Android Studio or VS Code. Or the command line:
+
+```console
+rajput-hemant@arch:~$ flutter pub get
+rajput-hemant@arch:~$ flutter run
+```
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daily Meals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Daily Meals'),
+      ),
+      body: const Center(
+        child: Text('Navigation Time!'),
+      ),
+    );
+  }
+}
