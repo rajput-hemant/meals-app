22eccaa6434993b779af5422e5faa0a924efd92e
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-06-01 18:19:10 +0530

    added meals title & a row which shows details of each meal
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -17,6 +17,28 @@ class MealItem extends StatelessWidget {
 
   void selectMeal() {}
 
+  String get getComplexity {
+    switch (complexity) {
+      case Complexity.simple:
+        return "Simple";
+      case Complexity.challenging:
+        return "Challenging";
+      case Complexity.hard:
+        return "Hard";
+    }
+  }
+
+  String get getAffordability {
+    switch (affordability) {
+      case Affordability.affordable:
+        return "Affordable";
+      case Affordability.pricey:
+        return "Pricey";
+      case Affordability.luxurious:
+        return "Expensive";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -42,9 +64,69 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
-                )
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 5,
+                  right: 5,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white70,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headline6,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.schedule),
+                        const SizedBox(width: 6),
+                        Text('$duration min'),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.extension),
+                        const SizedBox(width: 6),
+                        Text(getComplexity),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.currency_rupee),
+                        const SizedBox(width: 6),
+                        Text(getAffordability),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
